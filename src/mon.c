4f49d4a796d8c83e63dee5b67b8dea158607ab6f
Patric Mueller <bhaak@gmx.net>
2008-09-10 21:07:31 +0000

    fixes Bug 2103393

--- src/mon.c
+++ src/mon.c
@@ -2026,7 +2026,7 @@ int  typ, fatal;
 	    /* so have "poison arrow", "poison dart", etc... */
 	    plural = (string[strlen(string) - 1] == 's')? 1 : 0;
 	    /* avoid "The" Orcus's sting was poisoned... */
-	    pline("SUBJECT %s%s %s vergiftet!",  !isupper(string[1]) ? "" : "ARTIKEL_BESTIMMTER ", /* EN pline("%s%s %s poisoned!", isupper(*string) ? "" : "The ", */ // TODO DE richtiger Eigennamencheck
+	    pline("SUBJECT %s%s %s vergiftet!",  ((strncmp(string, "ARTIKEL_",8)==0) || !isupper(string[1])) ? "" : "ARTIKEL_BESTIMMTER ", /* EN pline("%s%s %s poisoned!", isupper(*string) ? "" : "The ", */ /* grep poisoned\( *.c, mthrowu.c:393 */
 			string, plural ? "waren" : "war"); /* EN string, plural ? "were" : "was"); */
 	}
 
